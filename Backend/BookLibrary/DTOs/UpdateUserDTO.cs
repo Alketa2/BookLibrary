434b8453e50aa6946e12a8482935522e12ffef98
Alketa2 <ramaalketa2406@gmail.com>
2025-08-29 23:48:40 +0200

    Backend folders - DTO

---
+++ Backend/BookLibrary/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookLibrary.DTOs.Request;
+
+public class UpdateUserDTO
+{
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+}
+
